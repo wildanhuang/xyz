26b1399aefefd6592f85463f9147a0bab75097d9
Wildan Prabowo <wildan.p@kiranatama.com>
2015-03-24 06:47:21 -0700

    add new clean repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xyz::Application.config.secret_token = 'dbd62a914a4e2b9a49717abd07394517f5f43eff5adc4578f67c5385414d8c0e47dd6fff3cce5780d53f0ff6d3133e9f7c0bc7581db1a2c3719172f674fd2291'
